9ebfb4c19df67fb4359c02b250fa6107aad8e442
Paul Irish <paul.irish@gmail.com>
2010-10-23 20:46:40 -0700

    move the scripts into /js/mylibs. wire things back up.

--- js/plugins.js
+++ js/plugins.js
@@ -1,18 +1,60 @@
 
-// remap jQuery to $
-(function($){
+
+
+
+
+
+
+  /**
+   * Converts an RGB color value to HSL. Conversion formula
+   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+   * Assumes r, g, and b are contained in the set [0, 255] and
+   * returns h, s, and l in the set [0, 1].
+   *
+   * @param   Number  r       The red color value
+   * @param   Number  g       The green color value
+   * @param   Number  b       The blue color value
+   * @return  Array           The HSL representation
+   */
+  function rgbToHsl(r, g, b){
+      r /= 255, g /= 255, b /= 255;
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+      var h, s, l = (max + min) / 2;
+
+      if(max == min){
+          h = s = 0; // achromatic
+      }else{
+          var d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+          switch(max){
+              case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+              case g: h = (b - r) / d + 2; break;
+              case b: h = (r - g) / d + 4; break;
+          }
+          h /= 6;
+      }
+
+      h = Math.round( h * 360);
+      s = Math.round( s * 100);
+      l = Math.round( l * 100);
+
+      return 'hsl(' + h + ',' + s + '%,' + l + '%)'; //[h, s, l]
+  }
+
+  function hex2hsl(hex){
+    hex = hex.replace('#','');
+    var rgb = parseInt(hex, 16); // value is 1675421
+
+    var red   = (rgb >> 16) & 0xFF; // returns 255
+    var green = (rgb >> 8) & 0xFF;  // 170
+    var blue  = rgb & 0xFF;		// 221
+
+    return rgbToHsl(red,green,blue);
+  }
+
 
  
 
-
-
-
-
- 
-
-
-
-})(this.jQuery);
 
 
 
@@ -25,16 +67,5 @@ window.log = function(){
     console.log( Array.prototype.slice.call(arguments) );
   }
 };
-
-
-
-// catch all document.write() calls
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
-})(document);
 
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,154 @@
 /* Author: 
+  ME, MOTHERFUCKER.
 
 */
+
+
+var h1 = document.querySelector('h1');
+
+
+// tell unsupporting browsers to FUCK OFF
+if (!'textShadow' in document.body.style){
+  document.querySelector('fieldset').style.display = 'none';
+  document.querySelector('textarea').style.display = 'none';
+  h1.style.marginTop = 0;
+  h1.innerHTML = 'Your browser doesn\'t support text-shadow. <small style="font-size: 20px">It was even in CSS2.0!</small> Just terrible...';
+}
+
+
+// possible values 1 or -1
+var offset = {
+  x : 1,
+  y : 1
+}
+
+var color = '#ffffff';
+
+var length = 11;
+var string = '';
+
+var wackyMode = false;
+
+var directionMap = {
+  SE : { x: 1 , y: 1 },
+  SW : { x: -1, y: 1 },
+  NW : { x: -1, y: -1},
+  NE : { x: 1 , y: -1}
+};
+
+var direction = 'SE';
+
+
+[].forEach.call( document.querySelectorAll('input[type="range"]') , function(elem, i){
+
+  elem.addEventListener('change',function(e){
+
+    // reset so we dont do multiple directions
+    if (!wackyMode){
+      [].forEach.call( document.querySelectorAll('input[type="range"]') , function(slider, i){
+        if (slider == elem) return;
+        slider.value = 0;
+      });
+    }
+
+    direction = elem.id.replace('len','');
+
+    length = e.target.value;
+
+    doIt();
+
+  },false);
+
+});
+
+
+// color
+function changeColor(e){
+  color = e.target.value;
+  doIt();
+};
+
+var elColor = document.querySelector('input[type="color"]');
+elColor.addEventListener('blur', changeColor, false);
+elColor.addEventListener('focus', changeColor, false);
+
+document.querySelector('input[type="checkbox"]').addEventListener('change', function(e){
+  wackyMode = e.target.checked;
+}, false);
+
+document.querySelector('button').addEventListener('click', function f(e){
+   f.on = !f.on;
+   e.target.innerHTML = f.on ? 'what does it mean?' : 'all the way';  
+   RAINBOWZ(f.on);
+}, false);
+
+function doIt(){
+
+  h1.style.textShadow = '';
+  string = '';
+
+  offset = directionMap[direction];
+
+  var sliders = document.querySelectorAll('input[type="range"]');
+
+  if (wackyMode){
+    for (var j = 0; j < sliders.length; j++){
+
+      offset = directionMap[sliders[j].id.replace('len','')];
+      var value = sliders[j].value;
+
+      for (var i = -1; ++i < value; ){
+       string += color +  ' ' + (i+1) * offset.x + 'px ' + (i+1) * offset.y + 'px,';
+      }
+    }
+  } else {
+    for (var i = -1; ++i < length; ){
+     string += color +  ' ' + (i+1) * offset.x + 'px ' + (i+1) * offset.y + 'px,';
+    }
+  }
+
+
+  string = string.slice(0,string.length-1);
+
+  setShadow(string);
+}
+
+doIt();
+
+
+function setShadow(string){
+  h1.style.textShadow = string;
+
+  document.querySelector('textarea').value = 'text-shadow: ' + string + ';';
+  
+}
+
+
+var RAINBOWZ = function(bool) {
+
+  if (!bool) {
+    doIt();
+    return;
+  }
+
+  var maxCount = 400;
+      shadows = '-1px -1px hsl(0,100%,50%)';
+
+  for ( var i = 1; i < maxCount; i++ ) {
+    var normI = i / maxCount,
+        hue = normI * 360 * 6,
+        x = parseInt( Math.sin( normI * Math.PI * 2 * 2 )  * 60 );
+
+    shadows += ', ' + x + 'px ' + i + 'px hsl(' + hue + ', 100%, 50%)'
+  }
+
+  setShadow(shadows)
+};
+
+
+
+
+
 
 
 
